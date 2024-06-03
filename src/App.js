d260fca36388d3eeb8a0467ce8f4afcf2b383d2f
Sophie Carr <sacarr98@gmail.com>
2024-06-03 06:13:37 +0000

    add navlinks to navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.module.css';
 import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className={StyleSheet.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home Page</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+          <Route render={() => <p>Page not found</p>} />
+        </Switch>
+
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,21 +4,43 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import logo from "../assets/fitnesslogo.png";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
-function NavBar () {
+const NavBar = () => {
   return (
     <Navbar expand="md" className={styles.NavBar} fixed="top">
       <Container>
-        <Navbar.Brand href="#home">
+        <Navbar.Brand>
             <img src={logo} alt="logo" height="45" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Profiles</Nav.Link>
-            <Nav.Link href="#link">Your Profile</Nav.Link>
+            <Nav.Link exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signin"
+              >
+                Sign In
+              </Nav.Link>
+              <Nav.Link exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signun"
+              >
+                Sign Up
+              </Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Your Progress</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">
+                Your Progress
+                </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Exercises
               </NavDropdown.Item>
