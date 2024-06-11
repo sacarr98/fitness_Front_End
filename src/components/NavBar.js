9ce03e4850419461da5e8f321424a7f7b33a2648
Sophie Carr <sacarr98@gmail.com>
2024-06-11 11:17:24 +0000

    install axios and connect workspace to backend

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,60 +1,48 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/fitnesslogo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" className={styles.NavBar} fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand>
             <img src={logo} alt="logo" height="45" />
-        </Navbar.Brand>
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link exact
+          <Nav className="ml-auto text-left">
+            <NavLink
+              exact
               className={styles.NavLink}
               activeClassName={styles.Active}
               to="/"
-              >
-                Home
-              </Nav.Link>
-              <Nav.Link exact
+            >
+              <i className="fas fa-home"></i>Home
+            </NavLink>
+            <NavLink
               className={styles.NavLink}
               activeClassName={styles.Active}
               to="/signin"
-              >
-                Sign In
-              </Nav.Link>
-              <Nav.Link exact
+            >
+              <i className="fas fa-sign-in-alt"></i>Sign in
+            </NavLink>
+            <NavLink
+              to="/signup"
               className={styles.NavLink}
               activeClassName={styles.Active}
-              to="/signun"
-              >
-                Sign Up
-              </Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">
-                Your Progress
-                </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Exercises
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Workouts</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Login/Out
-              </NavDropdown.Item>
-            </NavDropdown>
+            >
+              <i className="fas fa-user-plus"></i>Sign up
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
-}
+};
 
 export default NavBar;
